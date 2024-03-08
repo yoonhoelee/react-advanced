1f9b75497937e42746aecee5bd806ef305abb293
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-03-08 09:19:16 +0900

    feat: react hook form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,47 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import {useForm} from "react-hook-form";
 
+interface IForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    password1: string;
+}
 function ToDoList() {
-    const [toDo, setToDo] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = event;
-        setToDo(value);
-    };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(toDo);
-    };
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IForm>({
+        defaultValues: {
+            email: "@naver.com",
+        },
+    });
+    const onValid = (data: any) => {
+    }
+    console.log(watch());
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+            <form
+                style={{display: "flex", flexDirection: "column"}}
+                onSubmit={handleSubmit(onValid)}>
+                <input {...register("email", {
+                    required: true,
+                    minLength: 10,
+                    pattern: {
+                        value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                        message: "Only naver.com emails allowed",
+                    },
+                })} placeholder="Email"/>
+                <span>{errors?.email?.message}</span>
+                <input {...register("firstName", {required: true})} placeholder="First Name"/>
+                <input {...register("lastName", {required: true})} placeholder="Last Name"/>
+                <input {...register("username", {required: true, minLength: 10})} placeholder="Username"/>
+                <input {...register("password", {required: true, minLength: 5})} placeholder="Password"/>
+                <input {...register("password1", {required: true, minLength: 5})} placeholder="Password1"/>
                 <button>Add</button>
             </form>
         </div>
