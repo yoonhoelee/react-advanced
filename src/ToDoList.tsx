c9505b40bbe47b526cca1c98b8b16c256210d441
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-03-10 14:43:10 +0900

    feat: using recoil to globally maintain toDos

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,57 +1,19 @@
-import React, {useState} from "react";
-import {useForm} from "react-hook-form";
+import {useRecoilState, useRecoilValue} from "recoil";
+import CreateToDo from "./components/CreateToDo";
+import {toDoState} from "./components/atoms";
+import ToDo from "./components/ToDo";
 
-interface IForm {
-    email: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    password1: string;
-}
 
 function ToDoList() {
-    const {
-        register,
-        watch,
-        handleSubmit,
-        setError,
-        formState: {errors},
-    } = useForm<IForm>({
-        defaultValues: {
-            email: "@naver.com",
-        },
-    });
-    const onValid = (data: IForm) => {
-        if (data.password != data.password1) {
-            setError("password1", {message: "Passwords are not the same"},
-                {shouldFocus: true},
-            );
-        }
-    }
-    console.log(watch());
+    const toDos = useRecoilValue(toDoState);
     return (
         <div>
-            <form
-                style={{display: "flex", flexDirection: "column"}}
-                onSubmit={handleSubmit(onValid)}>
-                <input {...register("email", {
-                    required: true,
-                    minLength: 10,
-                    pattern: {
-                        value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                        message: "Only naver.com emails allowed",
-                    },
-                })} placeholder="Email"/>
-                <span>{errors?.email?.message}</span>
-                <input {...register("firstName", {required: true})} placeholder="First Name"/>
-                <input {...register("lastName", {required: true})} placeholder="Last Name"/>
-                <input {...register("username", {required: true, minLength: 10})} placeholder="Username"/>
-                <input {...register("password", {required: true, minLength: 5})} placeholder="Password"/>
-                <input {...register("password1", {required: true, minLength: 5})} placeholder="Password1"/>
-                <span>{errors?.password1?.message}</span>
-                <button>Add</button>
-            </form>
+            <h1>To Dos</h1>
+            <hr/>
+            <CreateToDo/>
+            <ul>
+                {toDos.map((toDo) => <ToDo key={toDo.id}{...toDo}/>)}
+            </ul>
         </div>
     );
 }
---
+++ src/components/ToDo.tsx
@@ -0,0 +1,33 @@
+import {IToDo, toDoState} from "./atoms";
+import {useSetRecoilState} from "recoil";
+
+function ToDo({text, category, id}: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+    };
+    return (
+        <li>
+            <span>{text}</span>
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    Done
+                </button>
+            )}
+        </li>
+    );
+}
+
+export default ToDo;
