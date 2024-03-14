3e33846cadedb20028dd9f4fefeb40f26d80fe32
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-03-14 12:09:40 +0900

    fix: use recoil selector and enums to refine code

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,11 +1,21 @@
 import {atom, selector} from "recoil";
 
+export enum Categories {
+    "TO_DO" = "TO_DO",
+    "DOING" = "DOING",
+    "DONE" = "DONE",
+}
+
 export interface IToDo {
     text: string;
     id: number;
-    category: "TO_DO" | "DOING" | "DONE";
+    category: Categories;
 }
 
+export const categoryState = atom<Categories>({
+    key:"category",
+    default:Categories.TO_DO
+})
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
@@ -15,10 +25,7 @@ export const toDoSelector = selector({
     key: "toDoSelector",
     get: ({ get }) => {
         const toDos = get(toDoState);
-        return [
-            toDos.filter((toDo) => toDo.category === "TO_DO"),
-            toDos.filter((toDo) => toDo.category === "DOING"),
-            toDos.filter((toDo) => toDo.category === "DONE"),
-        ];
+        const category = get(categoryState);
+        return toDos.filter((toDo) => toDo.category === category);
     },
 });
